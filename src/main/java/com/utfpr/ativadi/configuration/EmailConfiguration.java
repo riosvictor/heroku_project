9f435210a8896aeebb8c2e6eeba8709ed2bdad8c
Paulo Victor Rios <victorios.ba@gmail.com>
2020-05-20 08:07:07 -0300

    Update EmailConfiguration.java

--- src/main/java/com/utfpr/ativadi/configuration/EmailConfiguration.java
+++ src/main/java/com/utfpr/ativadi/configuration/EmailConfiguration.java
@@ -25,7 +25,9 @@ public class EmailConfiguration {
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.connectiontimeout", 5000);
+        props.put("mail.smtp.timeout", 5000);
+        props.put("mail.smtp.writetimeout", 5000);
 
         return mailSender;
     }
