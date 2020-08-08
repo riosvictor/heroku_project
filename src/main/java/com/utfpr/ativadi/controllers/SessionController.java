0f677690e3ba07a0142b7df0fdffabe334c007e9
Paulo Rios <paulo.pinto@trizy.com.br>
2020-08-08 10:33:00 -0300

    Alteração email recuperação
    Alteração email recuperação

--- src/main/java/com/utfpr/ativadi/controllers/SessionController.java
+++ src/main/java/com/utfpr/ativadi/controllers/SessionController.java
@@ -1,9 +1,14 @@
 package com.utfpr.ativadi.controllers;
 
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import com.utfpr.ativadi.component.EmailService;
 import com.utfpr.ativadi.entities.Mensagem;
 import com.utfpr.ativadi.entities.Usuario;
 import com.utfpr.ativadi.repositories.UsuarioRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,9 +20,11 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import javax.script.ScriptException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Optional;
 import java.lang.*;
+import com.sendgrid.*;
 
 
 @Controller
@@ -30,9 +37,10 @@ public class SessionController {
     private final String SUCESS = "sucessMessage";
     private final String INICIO = "index";
     private final String RECOVER_PASSWORD = "recover_password";
+    private static final Logger logger = LogManager.getLogger(SessionController.class);
 
-    @Autowired
-    public EmailService emailService;
+//    @Autowired
+//    public EmailService emailService;
 
     @Autowired
     public SessionController(UsuarioRepository usuarioRepository, AuditoriaController auditoria) {
@@ -83,17 +91,36 @@ public class SessionController {
     public String senderRecoveryEmail(Model model, @RequestParam("email") String email, HttpServletRequest request) {
         Optional<Usuario> usuario = usuarioRepository.findByEmail(email);
 
-        if(usuario.isPresent()){
-            emailService.sendEmail(Arrays.asList(usuario.get().getEmail())
-                                 , "Recuperação da Senha de Acesso"
-                                 , "Olá " + usuario.get().getNome() + ",\n" +
-                                         "Como solicitado através do ATIVA, estamos enviado a sua " +
-                                         "senha de acesso por meio do e-mail cadastrado por você.\n\n" +
-                                         "Senha: " + usuario.get().getSenha() + "\n\n" +
-                                         "Saudações da Equipe ATIVA!");
+        if (usuario.isPresent()){
+            Email from = new Email("ativasistema.edu@gmail.com");
+            String subject = "Recuperação da Senha de Acesso";
+            Email to = new Email(usuario.get().getEmail());
+            Content content = new Content("text/plain",
+                    "Olá " + usuario.get().getNome() + ",\n" +
+                            "Como solicitado através do ATIVA, estamos enviado a sua " +
+                            "senha de acesso por meio do e-mail cadastrado por você.\n\n" +
+                            "Senha: " + usuario.get().getSenha() + "\n\n" +
+                            "Saudações da Equipe ATIVA!");
+            Mail mail = new Mail(from, subject, to, content);
 
-            model.addAttribute(SUCESS, "O e-mail foi enviado com sucesso!");
-            return RECOVER_PASSWORD;
+            SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+            Request requestEmail = new Request();
+            try {
+                requestEmail.setMethod(Method.POST);
+                requestEmail.setEndpoint("mail/send");
+                requestEmail.setBody(mail.build());
+                Response response = sg.api(requestEmail);
+                logger.info(response.getStatusCode());
+                logger.info(response.getBody());
+                logger.info(response.getHeaders());
+                model.addAttribute(SUCESS, "O e-mail foi enviado com sucesso!");
+                return RECOVER_PASSWORD;
+            } catch (IOException ex) {
+                logger.error("Erro ao enviar o email");
+                logger.error(ex.getMessage());
+                model.addAttribute(ERROR, "Erro ao enviar o email!");
+                return RECOVER_PASSWORD;
+            }
         }else{
             model.addAttribute(ERROR, "O e-mail não foi encontrado!");
             return RECOVER_PASSWORD;
