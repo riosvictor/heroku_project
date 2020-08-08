9f77afe10380137daa360d0c5159430588d7cb6e
Paulo Victor Rios <victorios.ba@gmail.com>
2020-08-08 09:10:42 -0300

    Revert to Gmail

--- src/main/java/com/utfpr/ativadi/configuration/EmailConfiguration.java
+++ src/main/java/com/utfpr/ativadi/configuration/EmailConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-
 import java.util.Properties;
 
 @Configuration
@@ -17,11 +16,11 @@ public class EmailConfiguration {
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
-        mailSender.setHost("smtp-mail.outlook.com");
+        mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("ativasistema.edu@outlook.com");
-        mailSender.setPassword("ativautfpr2020");
+        mailSender.setUsername("ativasistema.edu@gmail.com");
+        mailSender.setPassword("ativautfpr@2020");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
